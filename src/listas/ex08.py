1c459e3a027667aa7f7bd23005f8f9041cd8a2b9
Pythonisk <pythonisk@gmail.com>
2022-04-08 23:09:04 -0300

    exercício 08

--- src/listas/ex08.py
+++ src/listas/ex08.py
@@ -2,3 +2,8 @@
 # Faça um programa que peça um número natural e mostre a tabuada de
 # multiplicar(1 a 10) desse número.
 # ------
+
+print('Programa TABUADA')
+num = int(input('Informe um número inteiro: '))
+for i in range(10):
+    print(f'{i+1} * {num} = {(i+1)*num}')
